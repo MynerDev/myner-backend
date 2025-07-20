d5d5b78bf2be6ba804977e50d6c3f05da8bbc76a
MynerDev <myner.dev@gmail.com>
2025-07-20 17:39:17 +0530

    DB connection change

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,11 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'admin',
-  database: process.env.DB_NAME || 'Myner',
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost:5432/Myner',
 });
 
 export const initializeDB = async () => {
